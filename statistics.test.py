98c2dafa68565caff4866ff2fc3752604e393865
Utkarsh Gupta <ututkarshgupta7@gmail.com>
2021-01-25 16:21:24 +0530

    Initial_Commit

--- statistics.test.py
+++ statistics.test.py
@@ -1,5 +1,25 @@
 import unittest
 import statistics
+import numpy as np
+
+class EmailAlert():
+    def __init__(self, threshold=0):
+        self.threshold = threshold
+    def emailSent():
+        return False 
+    
+class LEDAlert():
+    def ledGlows():
+        return True
+    
+class StatsAlerter(): 
+    def __init__(self, maxThreshold, object_li):
+        self.maxThreshold = maxThreshold
+        self.object_li = object_li
+    def checkAndAlert(self, threshold_li):
+        for threshold in threshold_li:
+            if threshold < self.maxThreshold:
+                self.object_li[0].__init__(threshold =threshold)
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -11,6 +31,10 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], np.nan, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], np.nan, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], np.nan, delta=epsilon)    
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
